750d568fd67d14e34ac85404fa9b74241324c838
Anastasia Tyryshkina <atyryshkina@gmail.com>
2021-04-05 20:38:36 -0400

    calling long de novo

--- short_tandem_repeats/de_novo_strs/format_and_filter.sh
+++ short_tandem_repeats/de_novo_strs/format_and_filter.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
 # concat all files
-ls output/monstr/ | grep locus_summary | grep -v chrX | grep -v all | while read l; do cat output/monstr/$l; done > output/monstr/all.locus_summary.tab
-ls output/monstr/ | grep locus_summary | grep chrX | grep -v all | while read l; do cat output/monstr/$l; done > output/monstr/all.chrX.locus_summary.tab
+ls output/monstr/ | grep locus_summary | grep -v chrX | grep -v all.all | while read l; do cat output/monstr/$l; done > output/monstr/all.locus_summary.tab
+ls output/monstr/ | grep locus_summary | grep chrX | grep -v all.all | while read l; do cat output/monstr/$l; done > output/monstr/all.chrX.locus_summary.tab
 ls output/monstr/ | grep all_mutations | grep -v chrX | grep -v all.all | while read l; do cat output/monstr/$l; done > output/monstr/all.all_mutations.tab
 ls output/monstr/ | grep all_mutations | grep chrX | grep -v all.all | while read l; do cat output/monstr/$l; done > output/monstr/all.chrX.all_mutations.tab
 
+# long STR calls
+ls output/monstr/long/ | grep locus_summary | grep -v chrX | grep -v all.all | while read l; do cat output/monstr/long/$l; done > output/monstr/long/all.locus_summary.tab
+ls output/monstr/long/ | grep locus_summary | grep chrX | grep -v all.all | while read l; do cat output/monstr/long/$l; done > output/monstr/long/all.chrX.locus_summary.tab
+ls output/monstr/long/ | grep all_mutations | grep -v chrX | grep -v all.all | while read l; do cat output/monstr/long/$l; done > output/monstr/long/all.all_mutations.tab
+ls output/monstr/long/ | grep all_mutations | grep chrX | grep -v all.all | while read l; do cat output/monstr/long/$l; done > output/monstr/long/all.chrX.all_mutations.tab
+
+
+
+#=================================================
 # filter for de novo strs
 # these scripts remove headers that are in the middle of the text file
 # and only keeps de novo mutations with posterior probability > 0.5
 python3 filter_de_novo_strs.py output/monstr/all.all_mutations.tab > output/filtered/all.all_mutations.filtered.tab
 python3 filter_de_novo_strs.py output/monstr/all.chrX.all_mutations.tab > output/filtered/all.chrX.all_mutations.filtered.tab
 
+# long STRs
+python3 filter_de_novo_strs_long.py output/monstr/long/all.all_mutations.tab > output/filtered/all.all_mutations.long.filtered.tab
+python3 filter_de_novo_strs_long.py output/monstr/long/all.chrX.all_mutations.tab > output/filtered/all.chrX.all_mutations.long.filtered.tab
+
+
+#=================================================
 
 # remove sex chromosomes from all mutations
 cat output/filtered/all.all_mutations.filtered.tab | awk '$1 != "chrX" && $1 != "chrY"' > output/filtered/all.all_mutations.filtered.no_sex.tab
@@ -19,6 +34,13 @@ cat output/filtered/all.all_mutations.filtered.tab | awk '$1 != "chrX" && $1 != 
 # append autosomal and X chromosome
 cat output/filtered/all.all_mutations.filtered.no_sex.tab > output/filtered/all.filtered.tab
 tail -n +2 output/filtered/all.chrX.all_mutations.filtered.tab >> output/filtered/all.filtered.tab
+
+# long STRs 
+# long STRsdon't have any sex chromosomes, so just move them to new file
+cat output/filtered/all.all_mutations.long.filtered.tab > output/filtered/all.long.filtered.tab
+
+
+#=================================================
 
 # filter out SG473 who has > 1000 mutations
 # and filter out SG136 whose mother SG138 WGS sample is bad
@@ -62,6 +84,7 @@ perl /data4/software/annovar/table_annovar.pl output/filtered/annovar_input.tsv 
 # add annovar annotations to original file
 outfilename=output/filtered/all.annotated.tab
 python3 append_annovar.py output/filtered/all.filtered4.tab output/filtered/annovar.hg19_multianno.txt $outfilename
+
 
 
 
